dd511f2ae2824de20a96bd9f77f2f6ba6dfc052a
Mia Celeste <mia@miaceleste.dev>
2022-02-12 21:51:01 -0800

    Set up porject

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    {{msg}}
+</template>
+
+<script lang="ts" setup>
+const msg="hi"
+</script>
+
+<style scoped lang="scss">
+$col: white;
+body{
+    background-color: $col;
+}
+</style>
