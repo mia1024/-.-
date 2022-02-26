0cb82a5d4e1ad22b74878a462354fefb6fd34db5
Kye Shi <shi.kye@gmail.com>
2022-02-25 21:46:34 -0800

    super basic stringify view

--- src/components/Code.vue
+++ src/components/Code.vue
@@ -1,11 +1,24 @@
 <template>
-    This is the placeholder for code section
+  <pre>{{ code }}</pre>
 </template>
 
 <script lang="ts" setup>
+import * as Store from "@/store";
+import * as Syntax from "@/lib/syntax";
+import * as Vue from "vue";
 
+const store = Store.store();
+
+const code = Vue.computed(() => {
+  const root = store.syntax.trail[store.syntax.trail.length - 1]!;
+  const tree = Syntax.structure(store.syntax.nodes, root);
+  return Syntax.stringifyCompact(tree);
+});
 </script>
 
 <style scoped lang="scss">
-
+pre {
+  margin: 1rem;
+  font-size: 1rem;
+}
 </style>
