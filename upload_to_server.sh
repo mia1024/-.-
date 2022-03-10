4130ab9956ccb4f06fecfec5086e57e757270d25
Mia Celeste <mia@miaceleste.dev>
2022-03-10 01:34:20 -0800

    Added upload to server script

---
+++ upload_to_server.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+print(){
+  printf "\033[38;5;93m"
+  printf "$1"
+  printf "\033[0m"
+}
+print "Typechecking..."
+yarn typecheck > /dev/null
+print "Done\n"
+
+print "Compiling..."
+yarn build > /dev/null
+print "Done\n"
+
+if [[ -z "$HMC_USERNAME" ]]; then
+  print "Enter your HMC server username (or set HMC_USERNAME environment variable): "
+  read username
+else
+  username=$HMC_USERNAME
+fi
+
+print "Uploading..."
+rsync -rua --delete dist/ "$username@cs.hmc.edu:~/public_html/lambda/"
+print "Website live at https://cs.hmc.edu/~$username/lambda (´｡• ω •｡\`)\n"
